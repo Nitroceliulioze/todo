990908df779527720bd4368b7d655f4ea2d4e3b2
Nitroceliulioze <aksionova.diana@gmail.com>
2023-09-29 13:45:26 +0300

    cleaned the code

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var savedTasks = JSON.parse(localStorage.getItem("tasks"));
+  const savedTasks = JSON.parse(localStorage.getItem("tasks"));
   if (savedTasks) {
-    var taskList = document.getElementById("taskList");
+    const taskList = document.getElementById("taskList");
     taskList.innerHTML = savedTasks;
   }
 });
 
 function addTask() {
-  var taskInput = document.getElementById("taskInput");
-  var taskList = document.getElementById("taskList");
+  const taskInput = document.getElementById("taskInput");
+  const taskList = document.getElementById("taskList");
 
   if (taskInput.value.trim() !== "") {
-    var li = document.createElement("li");
+    const li = document.createElement("li");
     li.innerHTML = `
         <input type="checkbox" onchange="toggleDone(this)">
         <span>${taskInput.value}</span>
         <div class='buttons-container'>
-            <button class='important-btn' onclick="markAsImportant(this)">Important</button>
-            <button class='del-btn' onclick="deleteTask(this)">Delete</button>
+            <button onclick="markAsImportant(this)">Important</button>
+            <button onclick="deleteTask(this)">Delete</button>
         </div>
       `;
     taskList.insertBefore(li, taskList.childNodes[0]);
@@ -29,15 +29,15 @@ function addTask() {
 }
 
 function toggleDone(checkbox) {
-    var span = checkbox.parentNode.querySelector("span");
+    const span = checkbox.parentNode.querySelector("span");
     span.classList.toggle("done");
 
     saveTasks();
   }
 
 function markAsImportant(button) {
-  var taskList = document.getElementById("taskList");
-  var li = button.parentNode.parentNode;
+  const taskList = document.getElementById("taskList");
+  const li = button.parentNode.parentNode;
 
   taskList.insertBefore(li, taskList.childNodes[0]);
 
@@ -53,8 +53,8 @@ function deleteTask(button) {
 }
 
 function saveTasks() {
-  var taskList = document.getElementById("taskList");
-  var tasks = taskList.innerHTML;
+  const taskList = document.getElementById("taskList");
+  const tasks = taskList.innerHTML;
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
